1c54897b091a3c25932955a82e5c725904ee8b77
Luca Benci <zero@develer.com>
2015-11-25 13:27:22 +0100

    Implementato check_user come decorator

--- tracker.py
+++ tracker.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from dbtool import connect_db
 from flask import Flask, jsonify, request, make_response, url_for, g
 from flask.ext.httpauth import HTTPBasicAuth
+from functools import wraps
+import os
 import sqlite3
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -32,21 +34,26 @@ def teardown_request(exception):
 
 @auth.verify_password
 def verify_password(username, password):
+    if not username:
+        return False
+
     user, b64pwd = g.db.execute("""select username, password from users
             where username == ?""", (username,)).fetchone()
+
     if not user or not cryptohelper.check_pwd(password, b64pwd):
         return False
+
     return True
 
-#@auth.get_password
-#def get_password(username):
-#    query = """select password from users
-#    where username == ?
-#    """
-#    password = g.db.execute(query, (username,)).fetchone()
-#    if password:
-#        return password[0]
-#    return
+
+def check_user(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = kwargs.get("user")
+        if user != auth.username() and user is not None:
+            return
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @auth.error_handler
@@ -63,25 +70,17 @@ def method_not_allowed():
 
 
 @app.errorhandler(500)
-def internal_server_error():
+def internal_server_error(exception):
     return make_response(
         jsonify({"message": "Internal server error"}), 500)
-
-
-def check_user(user, logged_in):
-    if user != logged_in and user is not None:
-        return False
-    return True
 
 
 # Aggiunta di un numero di passi per la giornata in corso
 @app.route("/v1/days", methods=["POST"])
 @app.route("/v2/users/<user>/days", methods=["POST"])
 @auth.login_required
+@check_user
 def add_day(user=None):
-    if not check_user(user, auth.username()):
-        return unauthorized()
-
     if not request.json or not "steps" in request.json:
         return make_response(
             jsonify({"message": "Bad Request: missing step number"}), 400)
@@ -114,10 +113,8 @@ def add_day(user=None):
 @app.route("/v1/days", methods=["PUT"])
 @app.route("/v2/users/<user>/days", methods=["PUT"])
 @auth.login_required
+@check_user
 def change_day(user=None):
-    if not check_user(user, auth.username()):
-        return unauthorized()
-
     if not request.json or not "steps" in request.json:
         make_response(
             jsonify({"message": "Bad Request: missing step number"}), 400)
@@ -152,10 +149,8 @@ def change_day(user=None):
 @app.route("/v1/days/<date>", methods=["GET"])
 @app.route("/v2/users/<user>/days/<date>", methods=["GET"])
 @auth.login_required
+@check_user
 def get_day(date, user=None):
-    if not check_user(user, auth.username()):
-        return unauthorized()
-
     day = get_date(date)
     if not day:
         return make_response(
@@ -173,10 +168,8 @@ def get_day(date, user=None):
 @app.route("/v1/days/<date>", methods=["DELETE"])
 @app.route("/v2/users/<user>/days/<date>", methods=["DELETE"])
 @auth.login_required
+@check_user
 def remove_day(date, user=None):
-    if not check_user(user, auth.username()):
-        return unauthorized()
-
     day = get_date(date)
     if not day:
         return make_response(jsonify({"message": "Not Found"}), 404)
@@ -220,14 +213,19 @@ def get_date(date):
     """
     Ritorna un giorno dal database
     """
-    return g.db.execute("""select date, nsteps from days
+    date_tup = g.db.execute("""select date, nsteps from days
                      where date == ? and username ==?""",
                      (date, auth.username())).fetchone()
+    if date_tup is None:
+        return None
+    return {"date": date_tup[0], "steps": date_tup[1]}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dbfile", help="database file name")
     args = parser.parse_args()
-    DBFILE = args.dbfile
+    DBFILE = args.dbfile or os.getenv("TRACKER_DB")
+    if not DBFILE:
+        exit("Please specify a database")
     app.run()
