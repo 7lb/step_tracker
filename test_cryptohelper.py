f328f2d2feb78216738588d93c19e985e656fde2
Luca Benci <zero@develer.com>
2015-11-25 13:25:43 +0100

    Aggiunto un test

--- test_cryptohelper.py
+++ test_cryptohelper.py
@@ -22,5 +22,16 @@ class CryptoTester(unittest.TestCase):
         self.assertTrue(cryptohelper.check_pwd(self.password, self.b64str,
             rounds=self.rounds))
 
+    def test_hash_pwd_different_times(self):
+        """
+        Controlla che due password che sono hashate un numero diverso di volte
+        producano una stringa finale diversa
+        """
+        # 0 e numeri negativi non sono supportati by design
+        for n in xrange(2, 10):
+            self.assertNotEqual(
+                cryptohelper.hash_pwd(self.password, self.salt, rounds=1),
+                cryptohelper.hash_pwd(self.password, self.salt, rounds=n))
+
 if __name__ == "__main__":
     unittest.main()
